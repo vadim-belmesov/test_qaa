e59b455a56f4e5c31b2faf46f5284320918474cf
Vadim <vadik.belmesov@yandex.ru>
2023-12-05 21:11:24 +0300

    Поиск элемента с помощью xpath

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,8 @@
 package org.example;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -10,10 +12,16 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.setProperty("webdriver.chrome.driver", "/home/balu/IdeaProjects/Test_QAA/web_driver/111/chromedriver");
-        WebDriver driver;
+//        WebDriver driver;
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        WebDriver driver = new ChromeDriver(); //экземпляр класса WebDriver
 
         driver.get("https://google.com");
+
+        WebElement button_search = driver.findElement(By.xpath("//input[@value='Поиск в Google']"));
+        WebElement settings_text = driver.findElement(By.xpath("//div[text()='Настройки']"));
+        //button_search.click();
+        settings_text.click();
+        //driver.quit();
     }
 }
