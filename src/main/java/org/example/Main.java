d5b880869a68f4e9ef4533bb0d83e48271d03dce
Vadim <vadik.belmesov@yandex.ru>
2023-12-08 23:26:38 +0300

    getText()

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,24 +5,22 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.time.Duration;
 public class Main {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "/home/balu/IdeaProjects/Test_QAA/web_driver/111/chromedriver");
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver(); //экземпляр класса WebDriver
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-        WebElement element = (new WebDriverWait(driver, Duration.ofSeconds(10))
-                .until(ExpectedConditions.presenceOfElementLocated(By.id("some_ID"))));
+        Duration duration = Duration.ofSeconds(10);
+        driver.manage().timeouts().implicitlyWait(duration);
 
-        driver.get("https://avito.ru");
+        driver.get("https://habr.ru/");
 
-        WebElement avtoRubrikatorButton = driver.findElement(By.xpath("//a[data-marker$='visual-rubricator/block-Авто']"));
-        avtoRubrikatorButton.click();
-        driver.close();
+        WebElement soglashenie = driver.findElement(By.xpath("//*[@class='tm-header__become-author-btn']"));
+        soglashenie.click();
+
+        String par = soglashenie.getText();
+        System.out.println(par);
     }
 }
